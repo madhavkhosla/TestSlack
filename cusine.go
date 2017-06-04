12dfe5062a43213ac9703c83402e319c8b4ba8ec
Madhav Khosla <madhav@Madhavs-MacBook-Pro.local>
2017-06-04 15:15:51 -0400

    Get Next working

--- cusine.go
+++ cusine.go
@@ -105,8 +105,52 @@ func GetNextFive(w http.ResponseWriter, r *http.Request) {
 	}
 	//newStart, _ := strconv.Atoi(interactiveRequestMessage.Actions[0].Value)
 
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, fmt.Sprintf("Count %s %s", v.CuisineId, v.LastCount))
+	ctx := appengine.NewContext(r)
+	lastCount, _ := strconv.Atoi(v.LastCount)
+	cusId, _ := strconv.Atoi(v.CuisineId)
+	restaurantNames, lastCount, err := GetRestaurantNamesInCityByCuisine(ctx, cusId, lastCount)
+	//restaurantNames, err := GetRestaurantNamesInCityByCuisine(inputCusineId)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	} else {
+
+		responseElements := make([]ResponseElement, 0)
+		for _, r := range restaurantNames {
+
+			//responseTextFormatted := fmt.Sprintf("Name: %s\n Address: %s\n Menu url: %s\n Cost for 2: %s\n, Rating: %s\n",
+			//	r.Name, r.Address, r.MenuUrl, r.AverageCostForTwo, r.AggregateRating)
+			responseElements = append(responseElements, ResponseElement{
+				Title:     fmt.Sprintf("%s", r.Name),
+				TitleLink: r.MenuUrl,
+				Fields:    r.Fields,
+				Color:     "#36a64f",
+				ThumbUrl:  r.ThumbUrl,
+			})
+		}
+		val, _ := json.Marshal(Value{LastCount: strconv.Itoa(lastCount), CuisineId: v.CuisineId})
+		action := Action{
+			Name:  "Get",
+			Text:  "Get",
+			Type:  "button",
+			Value: string(val)}
+		buttonAttachment := ResponseElement{Title: "Get next 5 restaurants", Actions: []Action{
+			action,
+		}, CallbackId: "zomato_next5", Color: "#36a64f"}
+		responseElements = append(responseElements, buttonAttachment)
+		response := SlackResponse{
+			ResponseType: "ephemeral",
+			Text:         fmt.Sprintf("The Restaurants for cuisine are "),
+			Attachments:  responseElements,
+		}
+		resp, err := json.Marshal(response)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "%s\n", resp)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, fmt.Sprintf("Count %s %s", v.CuisineId, v.LastCount))
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
